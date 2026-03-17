d251c90af3ebbf613bff426f432f069338951854
ThanhNguyen <nguyen5678212@gmail.com>
2026-03-17 23:30:02 +0700

    nopbai1

---
+++ Bai_1/bai1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class bai1
+{
+    static void Main()
+    {
+        Console.WriteLine("Ngo Thanh Nguyen_2415053122330");
+        List<int> numbers = new List<int>();
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write("Nhap so thu " + (i + 1) + ": ");
+            int n = int.Parse(Console.ReadLine());
+            numbers.Add(n);
+        }
+
+        foreach (int num in numbers)
+        {
+            Console.Write(num+" ");
+        }
+
+        Console.ReadLine();
+    }
+}
