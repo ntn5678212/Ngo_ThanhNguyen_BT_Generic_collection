1361ba5e0eb5cb991357c2c9931ecff88f0c42e2
ThanhNguyen <nguyen5678212@gmail.com>
2026-03-17 23:37:53 +0700

    nopbai5

---
+++ Bai_4/bai4.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+class bai4
+{
+    static void Main()
+    {
+        Console.WriteLine("Ngo Thanh Nguyen_2415053122330");
+        List<int> numbers = new List<int>();
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write("Nhap so thu " + (i + 1) + ": ");
+            int n = int.Parse(Console.ReadLine());
+            numbers.Add(n);
+        }
+
+        int count = 0;
+
+        foreach (int num in numbers)
+        {
+            if (num % 2 == 0)
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine("So luong so chan: " + count);
+
+        Console.ReadLine();
+    }
+}
---
+++ Bai_5/bai5.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Bai5
+{
+    static void Main()
+    {
+        Console.WriteLine("Ngo Thanh Nguyen_2415053122330");
+        List<string> students = new List<string>();
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write("Nhap ten sinh vien thu " + (i + 1) + ": ");
+            string name = Console.ReadLine();
+            students.Add(name);
+        }
+
+        Console.Write("Nhap ten sinh vien can xoa: ");
+        string removeName = Console.ReadLine();
+
+        students.Remove(removeName);
+
+        Console.WriteLine("Danh sach sinh vien sau khi xoa:");
+
+        foreach (string s in students)
+        {
+            Console.WriteLine(s);
+        }
+
+        Console.ReadLine();
+    }
+}
