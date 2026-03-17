9948a09e7bf1412491f5c0abc2db417af4550691
ThanhNguyen <nguyen5678212@gmail.com>
2026-03-17 23:42:13 +0700

    nopbai8

---
+++ Bai_3/bai3.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Bai3
+{
+    static void Main()
+    {
+        Console.WriteLine("Ngo Thanh Nguyen_2415053122330");
+        List<int> numbers = new List<int>();
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write("Nhap so thu " + (i + 1) + ": ");
+            int n = int.Parse(Console.ReadLine());
+            numbers.Add(n);
+        }
+
+        int max = numbers[0];
+
+        foreach (int num in numbers)
+        {
+            if (num > max)
+            {
+                max = num;
+            }
+        }
+
+        Console.WriteLine("So lon nhat la: " + max);
+
+        Console.ReadLine();
+    }
+}
